4a7735525447ba9d8cdf90ccbb509cb79828ecbc
Daniil Kolesnichenko <garlicbreadcleric@gmail.com>
2025-07-29 20:21:54 +0400

    Add Observatory sink

--- pkg/logger/sinks.go
+++ pkg/logger/sinks.go
@@ -1,8 +1,10 @@
 package logger
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"path/filepath"
 	"sync"
@@ -273,4 +275,57 @@ func (s *FilterSink) Write(msg Message) error {
 		return s.sink.Write(msg)
 	}
 	return nil
+}
+
+// ObservatorySink sends logs to an observatory service
+type ObservatorySink struct {
+	baseUri string
+}
+
+func NewObservatorySink(baseUri string) *ObservatorySink {
+	return &ObservatorySink{
+		baseUri: baseUri,
+	}
+}
+
+func (s *ObservatorySink) Write(msg Message) error {
+	body := ObservatoryPushLogsRequest{
+		Logs: []ObservatoryPushLogsRequestLog{
+			{
+				Message:   msg.Message,
+				LogLevel:  msg.Level,
+				Data:      msg.Context,
+				Module:    nil,
+				Submodule: nil,
+			},
+		},
+	}
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return fmt.Errorf("failed to marshal observatory log message: %w", err)
+	}
+
+	url := fmt.Sprintf("%s/api/v1/observatory/logs", s.baseUri)
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return fmt.Errorf("failed to send observatory log message: %w", err)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("observatory log request failed with status: %s", resp.Status)
+	}
+	return nil
+}
+
+type ObservatoryPushLogsRequest struct {
+	Logs []ObservatoryPushLogsRequestLog `json:"logs"`
+}
+
+type ObservatoryPushLogsRequestLog struct {
+	Message   string  `json:"message"`
+	LogLevel  string  `json:"logLevel"`
+	Data      any     `json:"data"`
+	Module    *string `json:"module"`
+	Submodule *string `json:"submodule"`
 }
